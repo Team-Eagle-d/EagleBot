040d2c7062cb06009369f10807c4befb9b630dcc
suil0304 <eagle030410@gmail.com>
2026-09-03 15:51:40 +0900

    feat: 공용 유틸리티 함수 추가
    Refs: #4

---
+++ apps/bot/src/utility/calc-performance.ts
@@ -0,0 +1,18 @@
+export async function calcPerformance<TResult, TArgs extends unknown[]>(
+    asyncFunc:(...args:TArgs) => Promise<TResult>,
+    ...args:TArgs
+):Promise<{
+    readonly result:TResult,
+    readonly performance:number
+}> {
+    const start = Date.now();
+
+    const result = await asyncFunc(...args);
+
+    const end = Date.now();
+
+    return {
+        result,
+        performance: end - start
+    };
+}
