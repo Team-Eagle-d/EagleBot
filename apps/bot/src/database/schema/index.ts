a9d91ca8cce7e0d3a4e57be2837815d0f2a74872
suil0304 <eagle030410@gmail.com>
2026-09-06 13:51:22 +0900

    refactor: 스키마 파일 이름 오타 수정
    Refs: #3

--- apps/bot/src/database/schema/index.ts
+++ apps/bot/src/database/schema/index.ts
@@ -1,4 +1,4 @@
 export { server } from "./server.schema.ts";
 export { serverUser } from "./server-user.schema.ts";
 export { user } from "./user.schema.ts";
-export { attendance } from "./attendance.scheme.ts";
+export { attendance } from "./attendance.schema.ts";
