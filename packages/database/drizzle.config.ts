167413400e184634642c1f455211128f7df54f1e
suil0304 <eagle030410@gmail.com>
2026-09-07 17:37:41 +0900

    feat: 실행 환경에 따라 위치가 변하지 않도록  drizzle.config.ts 내부 고정

--- packages/database/drizzle.config.ts
+++ packages/database/drizzle.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "drizzle-kit";
+import { join } from "@std/path";
 
 export default defineConfig({
-    schema: "./src/schema/*.schema.ts",
-    out: "./drizzle",
+    schema: join(import.meta.dirname, "./src/schema/*.schema.ts"),
+    out: join(import.meta.dirname, "./drizzle"),
     dialect: "postgresql",
     dbCredentials: {
         url: Deno.env.get("DATABASE_URL")!
